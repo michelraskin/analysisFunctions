55efe8da5541131031fc7dd0a97ee484a14c8844
michelraskin <43300264+michelraskin@users.noreply.github.com>
2024-11-03 15:04:45 -0600

    Replace all tabs to 4 spaces
    Works better with jupyter

--- analysis_functions.py
+++ analysis_functions.py
@@ -6,118 +6,118 @@ import time
 import os
 
 def q25(x):
-  return x.quantile(0.25)
+    return x.quantile(0.25)
 def q50(x):
-  return x.quantile(0.5)
+    return x.quantile(0.5)
 def q75(x):
-  return x.quantile(0.75)
+    return x.quantile(0.75)
 def q99(x):
-  return x.quantile(0.99)
+    return x.quantile(0.99)
 def q1(x):
-  return x.quantile(0.1)
+    return x.quantile(0.1)
 quantiles = [q1, q25, q50, q75, q99]
 quantilenames = ['q1', 'q25', 'q50', 'q75', 'q99']
 quantile_values = [0.01, 0.1, 0.25, 0.4, 0.5, 0.6, 0.75, 0.9, 0.99]
 
 def getDiffToPrev(aDataFrame, aType, aTypeName, aColumnFilter, aColumnCompare, shift=1):
-  if shift < 0:
-    myRelation = 'Next'
-  else:
-    myRelatio = 'Prev'
-  myColumn = aTypeName + 'Column'
-  myRelColumn = myRelation + myColumn
-  myDiffColumn = 'DiffTo' + myRelation + aTypeName
-  aDataFrame[myColumn] = aDataFrame[aColumnFilter].where(aType)
-  aDataFrame[myRelColumn] = aDataFrame[myColumn].ffill().shift(shift).fillna(0)
-  aDataFrame[myDiffColumn] = aDataFrame[aColumnCompare] - aDataFrame[myRelColumn]
-  return aDataFrame
+    if shift < 0:
+        myRelation = 'Next'
+    else:
+        myRelatio = 'Prev'
+    myColumn = aTypeName + 'Column'
+    myRelColumn = myRelation + myColumn
+    myDiffColumn = 'DiffTo' + myRelation + aTypeName
+    aDataFrame[myColumn] = aDataFrame[aColumnFilter].where(aType)
+    aDataFrame[myRelColumn] = aDataFrame[myColumn].ffill().shift(shift).fillna(0)
+    aDataFrame[myDiffColumn] = aDataFrame[aColumnCompare] - aDataFrame[myRelColumn]
+    return aDataFrame
 
 {
-  'Name': 'Name',
-  'ColumnFilter': 'ColumnFilter',
-  'ColumnCompare': 'ColumnCompare',
-  'Relation': 1,
-  'Filters': 
-  [
-    'OR',
+    'Name': 'Name',
+    'ColumnFilter': 'ColumnFilter',
+    'ColumnCompare': 'ColumnCompare',
+    'Relation': 1,
+    'Filters': 
     [
-      [
-        'AND',
+        'OR',
         [
-          {
-            'Name': 'ColumnName',
-            'Op': 'GEQ',
-            'Value': 12
-          }
-        ],
+            [
+                'AND',
+                [
+                    {
+                        'Name': 'ColumnName',
+                        'Op': 'GEQ',
+                        'Value': 12
+                    }
+                ],
 
-        [
-          {
-            'Name': 'ColumnName2',
-            'Op': 'EQ',
-            'Value': 15
-          }
+                [
+                    {
+                        'Name': 'ColumnName2',
+                        'Op': 'EQ',
+                        'Value': 15
+                    }
+                ]
+            ]
         ]
-      ]
     ]
-  ]
 }
 
 def fetchFilter(myDf, myFilter):
-  if myFilter['Op'] == 'LT':
-    return (myDf[myFilter['Name']] < myFilter['Value'])
-  if myFilter['Op'] == 'LEQ':
-    return (myDf[myFilter['Name']] <= myFilter['Value'])
-  if myFilter['Op'] == 'GT':
-    return (myDf[myFilter['Name']] > myFilter['Value'])
-  if myFilter['Op'] == 'GEQ':
-    return (myDf[myFilter['Name']] >= myFilter['Value'])
-  if myFilter['Op'] == 'EQ':
-    return (myDf[myFilter['Name']] == myFilter['Value'])
-  if myFilter['Op'] == 'NEQ':
-    return (myDf[myFilter['Name']] != myFilter['Value'])
-  if myFilter['Op'] == 'ISNA':
-    return (myDf[myFilter['Name']].isna() == True)
-  if myFilter['Op'] == 'NISNA':
-    return (myDf[myFilter['Name']].isna() == False)
-  
+    if myFilter['Op'] == 'LT':
+        return (myDf[myFilter['Name']] < myFilter['Value'])
+    if myFilter['Op'] == 'LEQ':
+        return (myDf[myFilter['Name']] <= myFilter['Value'])
+    if myFilter['Op'] == 'GT':
+        return (myDf[myFilter['Name']] > myFilter['Value'])
+    if myFilter['Op'] == 'GEQ':
+        return (myDf[myFilter['Name']] >= myFilter['Value'])
+    if myFilter['Op'] == 'EQ':
+        return (myDf[myFilter['Name']] == myFilter['Value'])
+    if myFilter['Op'] == 'NEQ':
+        return (myDf[myFilter['Name']] != myFilter['Value'])
+    if myFilter['Op'] == 'ISNA':
+        return (myDf[myFilter['Name']].isna() == True)
+    if myFilter['Op'] == 'NISNA':
+        return (myDf[myFilter['Name']].isna() == False)
+    
 def decodeFilterInfo(myDf, myFilter):
-  if len(myFilter) > 1:
-    myOp = myFilter[0]
-    myNewDf = pd.DataFrame()
-    for i, myValue in enumerate(myFilter[1]):
-      if i == 0:
-        if myOp == 'NOT':
-          myNewDf = ~(decodeFilterInfo(myDf, myValue))
-        else:
-          myNewDf = (decodeFilterInfo(myDf, myValue))
-      else:
-        if myOp == 'OR':
-          myNewDf = (myNewDf) | (decodeFilterInfo(myDf, myValue))
-        else:
-          myNewDf = (myNewDf) & (decodeFilterInfo(myDf, myValue))
-    return myNewDf
-  return fetchFilter(myDf, myFilter[0])
+    if len(myFilter) > 1:
+        myOp = myFilter[0]
+        myNewDf = pd.DataFrame()
+        for i, myValue in enumerate(myFilter[1]):
+            if i == 0:
+                if myOp == 'NOT':
+                    myNewDf = ~(decodeFilterInfo(myDf, myValue))
+                else:
+                    myNewDf = (decodeFilterInfo(myDf, myValue))
+            else:
+                if myOp == 'OR':
+                    myNewDf = (myNewDf) | (decodeFilterInfo(myDf, myValue))
+                else:
+                    myNewDf = (myNewDf) & (decodeFilterInfo(myDf, myValue))
+        return myNewDf
+    return fetchFilter(myDf, myFilter[0])
 
 def getSortedPerGroupInfo(myDfs, myDiffs, myTypes, myGroupColumn):
-  myDfs.fillna(0, inplace=True)
-  myDfsSorted = {}
-  for myGroup in myDfs[myGroupColumn].unique():
-    print(f'Running group: {myGroup}')
-    myDfsSorted[myGroup] = myDfs[myDfs[myGroupColumn] == myGroup].sort_values(['timestamp', 'offset'])
-    for myValue in myDiffs:
-      print(myValue) 
-      myDfsSorted[myGroup] = getDiffToPrev(myDfsSorted[myGroup], decodeFilterInfo(myDfsSorted[myGroup], myValue['Filters']), myValue['Name'], myValue['ColumnFilter'], myValue['ColumnCompare'], myValue['Relation'])
-    for myValue in myTypes:
-      print(myValue)
-      myDfsSorted[myGroup].loc[decodeFilterInfo(myDfsSorted[myGroup], myValue['Filters']), 'Type'] = myValue['Name']
-  return pd.concat(myDfsSorted.values())
+    myDfs.fillna(0, inplace=True)
+    myDfsSorted = {}
+    for myGroup in myDfs[myGroupColumn].unique():
+        print(f'Running group: {myGroup}')
+        myDfsSorted[myGroup] = myDfs[myDfs[myGroupColumn] == myGroup].sort_values(['timestamp', 'offset'])
+        for myValue in myDiffs:
+            print(myValue) 
+            myDfsSorted[myGroup] = getDiffToPrev(myDfsSorted[myGroup], decodeFilterInfo(myDfsSorted[myGroup], myValue['Filters']), myValue['Name'], myValue['ColumnFilter'], myValue['ColumnCompare'], myValue['Relation'])
+        for myValue in myTypes:
+            print(myValue)
+            myDfsSorted[myGroup].loc[decodeFilterInfo(myDfsSorted[myGroup], myValue['Filters']), 'Type'] = myValue['Name']
+    return pd.concat(myDfsSorted.values())
 
 def getPassedCount(myDf, myFilter):
-  myNewDf = myDf
-  myNewDf['Passed'] = decodeFilterInfo(myDf, myFilter)
-  myNewDf['PassedNum'] = myNewDf['Passed'].astype(int)
-  myNewDf = pd.DataFrame(myNewDf.gropuby(['Type']).agg({'Passed': 'sum', 'PassedNum': 'mean', 'offset': 'count'}))
-  myNewDf.sort_values('PassedNum', ascending = False, inplace=True)
-  myNewDf.rename(columns={'PassedNum': 'PassedPercent', 'Passed': 'PassedCount', 'offset': 'TotalCount'})
-  return myNewDf
+    myNewDf = myDf
+    myNewDf['Passed'] = decodeFilterInfo(myDf, myFilter)
+    myNewDf['PassedNum'] = myNewDf['Passed'].astype(int)
+    myNewDf = pd.DataFrame(myNewDf.gropuby(['Type']).agg({'Passed': 'sum', 'PassedNum': 'mean', 'offset': 'count'}))
+    myNewDf.sort_values('PassedNum', ascending = False, inplace=True)
+    myNewDf.rename(columns={'PassedNum': 'PassedPercent', 'Passed': 'PassedCount', 'offset': 'TotalCount'})
+    return myNewDf
--- machine_learning.py
+++ machine_learning.py
@@ -31,194 +31,170 @@ from scipy.stats import chi2
 from sklearn.decomposition import PCA
 
 DefaultGrid = [
-  {
-    'clf': [XGBClassifier(eval_metric='logloss'), RandomForestClassifier()],
-    'clf__n_estimators': [5, 10, 50], 
-    'clf__max_depth': [2, 5, 10]
-  },
-  {
-    'clf': [DecisionTreeClassifier()],
-    'clf__max_depth': [2, 5, 20],
-    'clf__class_weight': [None, 'balanced']
-  },
-  {
-    'clf': [LogisticRegression(), SVC(probability=True, kernel='linear')],
-    'clf__C': [0.1, 1, 10]
-  }
+    {
+        'clf': [XGBClassifier(eval_metric='logloss'), RandomForestClassifier()],
+        'clf__n_estimators': [5, 10, 50], 
+        'clf__max_depth': [2, 5, 10]
+    },
+    {
+        'clf': [DecisionTreeClassifier()],
+        'clf__max_depth': [2, 5, 20],
+        'clf__class_weight': [None, 'balanced']
+    },
+    {
+        'clf': [LogisticRegression(), SVC(probability=True, kernel='linear')],
+        'clf__C': [0.1, 1, 10]
+    }
 ]
 
 def getDefaultPreprocessor(aNumericalColumns, aBinaryColumns):
-  return  ColumnTransformer(
-      transformers=[
-        ('num', StandardScaler(), aNumericalColumns), 
-        ('bin', 'passthrough', aBinaryColumns), 
-      ],
-      remainder = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
-    )
+    return    ColumnTransformer(
+            transformers=[
+                ('num', StandardScaler(), aNumericalColumns), 
+                ('bin', 'passthrough', aBinaryColumns), 
+            ],
+            remainder = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
+        )
 
 def getDefaultPipelineSteps(X_train):
-  myNumericalColumns = X_train.columns[X_train.nunique() > 10]
-  myBinaryColumns = X_train.columns[X_train.nunique() == 2]
-  myPreprocessor = getDefaultPreprocessor(aNumericalColumns=myNumericalColumns, aBinaryColumns=myBinaryColumns)
-  return [('preprocessor', myPreprocessor), ('imputer', SimpleImputer(missing_values=np.nan, strategy='mean')), ('pca', PCA(n_components=0.9))]
+    myNumericalColumns = X_train.columns[X_train.nunique() > 10]
+    myBinaryColumns = X_train.columns[X_train.nunique() == 2]
+    myPreprocessor = getDefaultPreprocessor(aNumericalColumns=myNumericalColumns, aBinaryColumns=myBinaryColumns)
+    return [('preprocessor', myPreprocessor), ('imputer', SimpleImputer(missing_values=np.nan, strategy='mean')), ('pca', PCA(n_components=0.9))]
 
 def gridSearchKFoldClassification(X_train, X_test, y_train, y_test, aScore = 'roc_auc', aGrid = DefaultGrid):
-  kf = StratifiedKFold(n_splits=10, shuffle=True)
-  myPipelineSteps = getDefaultPipelineSteps(X_train = X_train)
-  myPipelineSteps.append(('clf', XGBClassifier()))
-  myPipeline = Pipeline(myPipelineSteps)
-  myGridSearchCv = GridSearchCV(myPipeline, aGrid, cv=kf, scoring=aScore, n_jobs=-1, verbose=0)
-  myGridSearchCv.fit(X_train, y_train)
-  myBestModel = myGridSearchCv.best_estimator_
-  y_pred_proba = myBestModel.predict_proba(X_test)[:, 1]
-  y_pred = myBestModel.predict(X_test)
-  print(f'Best parameters: {myGridSearchCv.best_params_}')
-  print(f'Best cross val {aScore} score: {myGridSearchCv.best_score_:.4f}')
-  print(f'Area under the receiver operating curve on test set is {roc_auc_score(y_test, y_pred_proba):.4f}')
-  print(f'Accuracy score on test set is {accuracy_score(y_test, y_pred):.4f}')
-  return myGridSearchCv
+    kf = StratifiedKFold(n_splits=10, shuffle=True)
+    myPipelineSteps = getDefaultPipelineSteps(X_train = X_train)
+    myPipelineSteps.append(('clf', XGBClassifier()))
+    myPipeline = Pipeline(myPipelineSteps)
+    myGridSearchCv = GridSearchCV(myPipeline, aGrid, cv=kf, scoring=aScore, n_jobs=-1, verbose=0)
+    myGridSearchCv.fit(X_train, y_train)
+    myBestModel = myGridSearchCv.best_estimator_
+    y_pred_proba = myBestModel.predict_proba(X_test)[:, 1]
+    y_pred = myBestModel.predict(X_test)
+    print(f'Best parameters: {myGridSearchCv.best_params_}')
+    print(f'Best cross val {aScore} score: {myGridSearchCv.best_score_:.4f}')
+    print(f'Area under the receiver operating curve on test set is {roc_auc_score(y_test, y_pred_proba):.4f}')
+    print(f'Accuracy score on test set is {accuracy_score(y_test, y_pred):.4f}')
+    return myGridSearchCv
 
 def getTopFeatures(aGridSearch, aColumnNames, X_train= None, y_train = None):
-  myBestModel = aGridSearch.best_estimator_
-  if hasattr(myBestModel.named_steps['clf'], 'coef_'):
-    myImportances = myBestModel.named_steps['clf'].coef_[0]
-  elif hasattr(myBestModel.named_steps['clf'], 'feature_importances_'):
-    myImportances = myBestModel.named_steps['clf'].feature_importances_
-  else: 
-    myImportances = permutation_importance(myBestModel, X_train, y_train).importances_mean
-    aColumnNames = X_train.columns
-  
-  myFeatureImportancesDf = pd.DataFrame({
-    'Feature': aColumnNames,
-    'Importance': myImportances
-  })
-  myFeatureImportancesDf.sort_values(by='Importance', ascending=False, inplace=True)
-  myTopFeatures = myFeatureImportancesDf.head(50)
-  plt.figure(figsize=(8, 8))
-  sns.barplot(x = 'Importance', y= 'Feature', data=myTopFeatures)
-  plt.title(f'Importances for {aGridSearch.best_params_}')
-  return myFeatureImportancesDf
+    myBestModel = aGridSearch.best_estimator_
+    if hasattr(myBestModel.named_steps['clf'], 'coef_'):
+        myImportances = myBestModel.named_steps['clf'].coef_[0]
+    elif hasattr(myBestModel.named_steps['clf'], 'feature_importances_'):
+        myImportances = myBestModel.named_steps['clf'].feature_importances_
+    else: 
+        myImportances = permutation_importance(myBestModel, X_train, y_train).importances_mean
+        aColumnNames = X_train.columns
+    
+    myFeatureImportancesDf = pd.DataFrame({
+        'Feature': aColumnNames,
+        'Importance': myImportances
+    })
+    myFeatureImportancesDf.sort_values(by='Importance', ascending=False, inplace=True)
+    myTopFeatures = myFeatureImportancesDf.head(50)
+    plt.figure(figsize=(8, 8))
+    sns.barplot(x = 'Importance', y= 'Feature', data=myTopFeatures)
+    plt.title(f'Importances for {aGridSearch.best_params_}')
+    return myFeatureImportancesDf
 
 DefaulGridRegression = [
-  {
-    'clf': [XGBRegressor(), RandomForestRegressor()],
-    'clf__n_estimators': [5, 10, 200], 
-    'clf__max_depth': [2, 5, 10, None]
-  },
-  {
-    'clf': [KNeighborsRegressor()],
-    'clf__n_neighbors': [2, 5, 10],
-  },
-  {
-    'clf': [LogisticRegression(), SVR()],
-    'clf__C': [0.1, 1, 10]
-  }
+    {
+        'clf': [XGBRegressor(), RandomForestRegressor()],
+        'clf__n_estimators': [5, 10, 200], 
+        'clf__max_depth': [2, 5, 10, None]
+    },
+    {
+        'clf': [KNeighborsRegressor()],
+        'clf__n_neighbors': [2, 5, 10],
+    },
+    {
+        'clf': [LogisticRegression(), SVR()],
+        'clf__C': [0.1, 1, 10]
+    }
 ]
 
 def gridSearchKFoldRegression(X_train, X_test, y_train, y_test, aScore = 'r2', aGrid = DefaulGridRegression):
-  kf = StratifiedKFold(n_splits=5, shuffle=True)
-  
-  myPipelineSteps = getDefaultPipelineSteps(X_train=X_train)
-  myPipelineSteps.append(('clf', XGBRegressor()))
-  myPipeline = Pipeline(myPipelineSteps)
-  myGridSearchCv = GridSearchCV(myPipeline, aGrid, cv=kf, scoring=aScore, n_jobs=-1, verbose=0)
-  myGridSearchCv.fit(X_train, y_train)
-  myBestModel = myGridSearchCv.best_estimator_
-  y_pred = myBestModel.predict(X_test)
-  print(f'Best parameters: {myGridSearchCv.best_params_}')
-  print(f'Best cross val {aScore} score: {myGridSearchCv.best_score_:.4f}')
-  print(f'R2 score  on test set: {r2_score(y_test, y_pred)}')
-  return myGridSearchCv
+    kf = StratifiedKFold(n_splits=5, shuffle=True)
+    
+    myPipelineSteps = getDefaultPipelineSteps(X_train=X_train)
+    myPipelineSteps.append(('clf', XGBRegressor()))
+    myPipeline = Pipeline(myPipelineSteps)
+    myGridSearchCv = GridSearchCV(myPipeline, aGrid, cv=kf, scoring=aScore, n_jobs=-1, verbose=0)
+    myGridSearchCv.fit(X_train, y_train)
+    myBestModel = myGridSearchCv.best_estimator_
+    y_pred = myBestModel.predict(X_test)
+    print(f'Best parameters: {myGridSearchCv.best_params_}')
+    print(f'Best cross val {aScore} score: {myGridSearchCv.best_score_:.4f}')
+    print(f'R2 score    on test set: {r2_score(y_test, y_pred)}')
+    return myGridSearchCv
 
 def plotRocAucCuve(aGridSearchCv, X_test, y_test):
-  myBestModel = aGridSearchCv.best_estimator_
-  y_pred_proba = myBestModel.predict_proba(X_test)[:, 1]
-  myFpr, myTpr, myThresholds = roc_curve(y_test, y_pred_proba)
-  myRocAuc = auc(myFpr, myTpr)
-  sns.set(style='whitegrid')
-  plt.figure(figsize=(8,6))
-  sns.scatterplot(x= myFpr, y= myTpr, color='darkorange', lw=2, label = f'ROC curve {myRocAuc:.2f}')
-  sns.lineplot(x= [0, 1], y= [0,1], color='navy', lw=2, linestyle = '--')
-  plt.xlim([0, 1.0])
-  plt.ylim([0, 1.05])
-  plt.xlabel('False positive rate')
-  plt.ylabel('True Positive Rate')
-  plt.legend(loc='lower right')
-  plt.title(f'ROC Curve for {aGridSearchCv.best_params_}')
-  plt.show()
+    myBestModel = aGridSearchCv.best_estimator_
+    y_pred_proba = myBestModel.predict_proba(X_test)[:, 1]
+    myFpr, myTpr, myThresholds = roc_curve(y_test, y_pred_proba)
+    myRocAuc = auc(myFpr, myTpr)
+    sns.set(style='whitegrid')
+    plt.figure(figsize=(8,6))
+    sns.scatterplot(x= myFpr, y= myTpr, color='darkorange', lw=2, label = f'ROC curve {myRocAuc:.2f}')
+    sns.lineplot(x= [0, 1], y= [0,1], color='navy', lw=2, linestyle = '--')
+    plt.xlim([0, 1.0])
+    plt.ylim([0, 1.05])
+    plt.xlabel('False positive rate')
+    plt.ylabel('True Positive Rate')
+    plt.legend(loc='lower right')
+    plt.title(f'ROC Curve for {aGridSearchCv.best_params_}')
+    plt.show()
 
 def getPredictedThirds(aDf):
-  lower_third = aDf['predicted_effect'].quantile(1/3)
-  upper_third = aDf['predicted_effect'].quantile(2/3)
-  if upper_third == lower_third:
-    print(f'No effect difference')
-    return 1, 1, aDf
-  aDf['predicted_effect_group'] = pd.cut(
-    aDf['predicted_effect'],
-    bins=[-float('inf'), lower_third, upper_third, float('inf')],
-    labels=['Lower', 'Middle', 'Upper']
-  )
-  return lower_third, upper_third, aDf
+    lower_third = aDf['predicted_effect'].quantile(1/3)
+    upper_third = aDf['predicted_effect'].quantile(2/3)
+    if upper_third == lower_third:
+        print(f'No effect difference')
+        return 1, 1, aDf
+    aDf['predicted_effect_group'] = pd.cut(
+        aDf['predicted_effect'],
+        bins=[-float('inf'), lower_third, upper_third, float('inf')],
+        labels=['Lower', 'Middle', 'Upper']
+    )
+    return lower_third, upper_third, aDf
 
 def getPredictedTreatmentEffectSupervisedClassif(X_train, aModel):
-  myXValueModified1 = X_train.copy()
-  myXValueModified1['groupe'] = 1.0
-  myXValueModified2 = X_train.copy()
-  myXValueModified2['groupe'] = 0.0
-  y_pred_proba1 = aModel.predict_proba(myXValueModified1)[:, 1]
-  y_pred_proba2 = aModel.predict_proba(myXValueModified2)[:, 1]
-  myNewDf = pd.DataFrame()
-  myNewDf['predicted_effect'] = (y_pred_proba1 - y_pred_proba2)
-  lower_third, upper_third, myNewDf = getPredictedThirds(myNewDf)
-  if lower_third == upper_third:
-    plt.scatter(x = range(len(y_pred_proba1)), y = myNewDf['predicted_effect'].sort_values())
-  else:
-    myNewDf.sort_values(['predicted_effect'], inplace=True)
-    myNewDf.reset_index(inplace=True)
-    for group in myNewDf['predicted_effect_group']:
-      myFilter = myNewDf['predicted_effect_group'] == group
-      plt.scatter(x = myNewDf[myFilter]['predicted_effect'].index, y = myNewDf[myFilter]['predicted_effect'])
-    plt.legend(myNewDf['predicted_effect_group'].unique())
-  return lower_third, upper_third, myNewDf
-
-def getTreatmentEffectDiff(X_train, y_train, aModel):
-  lower_third, upper_third, myNewDf = getPredictedTreatmentEffectSupervisedClassif(X_train, aModel)
-  if upper_third == lower_third:
-    print(f'No effect difference')
-    return 1
-  myData = pd.concat([X_train['groupe'], myNewDf['predicted_effect_group'], y_train], axis=1)
-  model1 = smf.logit(
-    'CPC12 ~ predicted_effect_group + groupe',
-    data=myData
-  ).fit()
-
-  model2 = smf.logit(
-    'CPC12 ~ predicted_effect_group * groupe',
-    data=myData
-  ).fit()
-
-  llr = -2*(model1.llf - model2.llf)
-  df_diff = model2.df_model - model1.df_model
-  p_value = chi2.sf(llr, df_diff)
-
-  print(f'Likelihood ratio of test results:')
-  print(f'Chi square statistic: {llr}')
-  print(f'p-value: {p_value}')
-  print(f'Degress of freedom: {df_diff}')
-  return p_value
-
-def getTreatmentEffectDiffUnsupervised(aX, aY, aGroups, aCategory = 'CPC12'):
+    myXValueModified1 = X_train.copy()
+    myXValueModified1['groupe'] = 1.0
+    myXValueModified2 = X_train.copy()
+    myXValueModified2['groupe'] = 0.0
+    y_pred_proba1 = aModel.predict_proba(myXValueModified1)[:, 1]
+    y_pred_proba2 = aModel.predict_proba(myXValueModified2)[:, 1]
     myNewDf = pd.DataFrame()
-    myNewDf['predicted_effect_group'] = aGroups
-    myData = pd.concat([aX['groupe'], myNewDf['predicted_effect_group'], aY], axis=1)
+    myNewDf['predicted_effect'] = (y_pred_proba1 - y_pred_proba2)
+    lower_third, upper_third, myNewDf = getPredictedThirds(myNewDf)
+    if lower_third == upper_third:
+        plt.scatter(x = range(len(y_pred_proba1)), y = myNewDf['predicted_effect'].sort_values())
+    else:
+        myNewDf.sort_values(['predicted_effect'], inplace=True)
+        myNewDf.reset_index(inplace=True)
+        for group in myNewDf['predicted_effect_group']:
+            myFilter = myNewDf['predicted_effect_group'] == group
+            plt.scatter(x = myNewDf[myFilter]['predicted_effect'].index, y = myNewDf[myFilter]['predicted_effect'])
+        plt.legend(myNewDf['predicted_effect_group'].unique())
+    return lower_third, upper_third, myNewDf
+
+def getTreatmentEffectDiff(X_train, y_train, aModel, aCategory = 'CPC12'):
+    lower_third, upper_third, myNewDf = getPredictedTreatmentEffectSupervisedClassif(X_train, aModel)
+    if upper_third == lower_third:
+        print(f'No effect difference')
+        return 1
+    myData = pd.concat([X_train['groupe'], myNewDf['predicted_effect_group'], y_train], axis=1)
     model1 = smf.logit(
-    f'{aCategory} ~ predicted_effect_group + groupe',
-    data=myData
+        f'{aCategory} ~ predicted_effect_group + groupe',
+        data=myData
     ).fit()
 
     model2 = smf.logit(
-    f'{aCategory} ~ predicted_effect_group * groupe',
-    data=myData
+        f'{aCategory} ~ predicted_effect_group * groupe',
+        data=myData
     ).fit()
 
     llr = -2*(model1.llf - model2.llf)
@@ -229,50 +205,74 @@ def getTreatmentEffectDiffUnsupervised(aX, aY, aGroups, aCategory = 'CPC12'):
     print(f'Chi square statistic: {llr}')
     print(f'p-value: {p_value}')
     print(f'Degress of freedom: {df_diff}')
-    return p_value, model2, myData
+    return p_value
+
+def getTreatmentEffectDiffUnsupervised(aX, aY, aGroups, aCategory = 'CPC12'):
+        myNewDf = pd.DataFrame()
+        myNewDf['predicted_effect_group'] = aGroups
+        myData = pd.concat([aX['groupe'], myNewDf['predicted_effect_group'], aY], axis=1)
+        model1 = smf.logit(
+        f'{aCategory} ~ predicted_effect_group + groupe',
+        data=myData
+        ).fit()
+
+        model2 = smf.logit(
+        f'{aCategory} ~ predicted_effect_group * groupe',
+        data=myData
+        ).fit()
+
+        llr = -2*(model1.llf - model2.llf)
+        df_diff = model2.df_model - model1.df_model
+        p_value = chi2.sf(llr, df_diff)
+
+        print(f'Likelihood ratio of test results:')
+        print(f'Chi square statistic: {llr}')
+        print(f'p-value: {p_value}')
+        print(f'Degress of freedom: {df_diff}')
+        return p_value, model2, myData
 
 def plotPredictedEffectDiff(aData, aBestModel):
-  predicted_effect_groups = aData['predicted_effect_group'].unique()
-  predicted_effect_groups.sort()
-  groupe_values = aData['groupe'].unique()
-  groupe_values.sort()
-  groupe_values = list(filter(lambda x: not np.isnan(x), groupe_values))
-  
-  predicted_effect_groups = list(filter(lambda x: not np.isnan(x), predicted_effect_groups))
+    predicted_effect_groups = aData['predicted_effect_group'].unique()
+    predicted_effect_groups.sort()
+    groupe_values = aData['groupe'].unique()
+    groupe_values.sort()
+    groupe_values = list(filter(lambda x: not np.isnan(x), groupe_values))
+    
+    predicted_effect_groups = list(filter(lambda x: not np.isnan(x), predicted_effect_groups))
 
-  # Prepare the DataFrame for prediction
-  predictions = []
-  for groupe in groupe_values:
-      for effect in predicted_effect_groups:
-          temp_df = pd.DataFrame({
-              'predicted_effect_group': [effect],
-              'groupe': [groupe]
-          })
-          # Predict the probability
-          temp_df['predicted_prob'] = aBestModel.predict(temp_df)
-          predictions.append(temp_df)
+    # Prepare the DataFrame for prediction
+    predictions = []
+    for groupe in groupe_values:
+            for effect in predicted_effect_groups:
+                    temp_df = pd.DataFrame({
+                            'predicted_effect_group': [effect],
+                            'groupe': [groupe]
+                    })
+                    # Predict the probability
+                    temp_df['predicted_prob'] = aBestModel.predict(temp_df)
+                    predictions.append(temp_df)
 
-  # Concatenate all predictions
-  predictions_df = pd.concat(predictions)
+    # Concatenate all predictions
+    predictions_df = pd.concat(predictions)
 
-  # Create a bar plot
-  plt.figure(figsize=(8, 6))
-  for i, groupe in enumerate(groupe_values):
-      subset = predictions_df[predictions_df['groupe'] == groupe]
-      plt.bar(
-          subset['predicted_effect_group'] + (i * 0.2) - 0.1,  # Shift bars slightly for better visualization
-          subset['predicted_prob'],
-          width=0.2,
-          label=f'Groupe {groupe}'
-      )
-      
-  x_positions = np.arange(len(predicted_effect_groups)) 
+    # Create a bar plot
+    plt.figure(figsize=(8, 6))
+    for i, groupe in enumerate(groupe_values):
+            subset = predictions_df[predictions_df['groupe'] == groupe]
+            plt.bar(
+                    subset['predicted_effect_group'] + (i * 0.2) - 0.1,    # Shift bars slightly for better visualization
+                    subset['predicted_prob'],
+                    width=0.2,
+                    label=f'Groupe {groupe}'
+            )
+            
+    x_positions = np.arange(len(predicted_effect_groups)) 
 
-  plt.xticks(x_positions)
-  plt.xlabel('Predicted Effect Group')
-  plt.ylabel('Predicted Probability of CPC12')
-  plt.title('Predicted Probability of CPC12 by Predicted Effect Group and Groupe')
-  plt.legend(title='Groupe')
-  plt.grid(True, axis='y', linestyle='--', alpha=0.7)
-  plt.tight_layout()
-  plt.show()
+    plt.xticks(x_positions)
+    plt.xlabel('Predicted Effect Group')
+    plt.ylabel('Predicted Probability of CPC12')
+    plt.title('Predicted Probability of CPC12 by Predicted Effect Group and Groupe')
+    plt.legend(title='Groupe')
+    plt.grid(True, axis='y', linestyle='--', alpha=0.7)
+    plt.tight_layout()
+    plt.show()
